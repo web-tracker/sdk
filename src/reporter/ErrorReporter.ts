29e5e3485ff8dca369edaa817e368828b89e2c6d
ele828 <dongjiemusic@qq.com>
2017-05-12 21:26:08 +0800

    update

--- src/reporter/ErrorReporter.ts
+++ src/reporter/ErrorReporter.ts
@@ -3,48 +3,48 @@ import { Environment } from '../detector/EnvironmentDetector';
 import BufferedReporter from './BufferedReporter';
 
 export default class ErrorReporter extends BufferedReporter<Error> {
-  private endpoint: string = `//127.0.0.1:3000/error.gif`;
+    private endpoint: string = `//www.dobest.me:3030/error.gif`;
 
-  constructor(private token: string, private environment: Environment) { super(); }
+    constructor(private token: string, private environment: Environment) { super(); }
 
-  report(error: Error): void {
-    this.bufferReport(error);
-  }
-
-  _report() {
-    LOG('Report Error:', this.reportQueue);
-    const parameters = this.parameterBuilder(this.reportQueue);
-    LOG('Error Parameters:', parameters);
-    const url = this.URLBuilder(this.endpoint, parameters);
-    return this.sendRequest(url);
-  }
-
-  /**
-   * If there are already errors in the queue,
-   * we just ignore this error.
-   */
-  _merge(error: Error) {
-    const found = this.reportQueue.filter(
-      errorInQueue => error.stack === errorInQueue.stack
-    );
-    if (found.length <= 0) {
-      this.reportQueue.push(error);
+    report(error: Error): void {
+        this.bufferReport(error);
     }
-  }
 
-  parameterBuilder(errors: Error[]) {
-    let parameters = {};
-    parameters['err'] = JSON.stringify(errors);
-    // Should also carry token string
-    parameters['token'] = this.token;
+    _report() {
+        LOG('Report Error:', this.reportQueue);
+        const parameters = this.parameterBuilder(this.reportQueue);
+        LOG('Error Parameters:', parameters);
+        const url = this.URLBuilder(this.endpoint, parameters);
+        return this.sendRequest(url);
+    }
 
-    // Combine environment infomation
-    parameters['os'] = this.environment.operatingSystem._type.toLowerCase();
-    parameters['br'] = this.environment.browser._type.toLowerCase();
-    parameters['bv'] = this.environment.browser.version.toLowerCase();
-    parameters['dc'] = this.environment.device.type;
-    parameters['dv'] = this.environment.device.version;
-    return parameters;
-  }
+    /**
+     * If there are already errors in the queue,
+     * we just ignore this error.
+     */
+    _merge(error: Error) {
+        const found = this.reportQueue.filter(
+            errorInQueue => error.stack === errorInQueue.stack
+        );
+        if (found.length <= 0) {
+            this.reportQueue.push(error);
+        }
+    }
+
+    parameterBuilder(errors: Error[]) {
+        let parameters = {};
+        parameters['err'] = JSON.stringify(errors);
+        // Should also carry token string
+        parameters['token'] = this.token;
+
+        // Combine environment infomation
+        parameters['os'] = this.environment.operatingSystem._type.toLowerCase();
+        parameters['br'] = this.environment.browser._type.toLowerCase();
+        parameters['bv'] = this.environment.browser.version.toLowerCase();
+        parameters['dc'] = this.environment.device.type;
+        parameters['dv'] = this.environment.device.version;
+        return parameters;
+    }
 
 }
--- src/reporter/PerformanceReporter.ts
+++ src/reporter/PerformanceReporter.ts
@@ -3,57 +3,57 @@ import { Metric } from '../metric/BaseMetric';
 import { Environment } from '../index';
 
 const parameterMapper = {
-  firstPaintTime: 'fp',
-  firstByteTime: 'fb',
-  firstMeaningfulTime: 'fm',
-  firstInteractionTime: 'fi',
-  totalLoadingTime: 'tl',
-  downloadingTime: 'dl',
-  DOMParsingTime: 'dp',
-  DNSLookupTime: 'du',
-  imagesTime: 'im',
-  stylesTime: 'ss',
-  scriptsTime: 'sc'
+    firstPaintTime: 'fp',
+    firstByteTime: 'fb',
+    firstMeaningfulTime: 'fm',
+    firstInteractionTime: 'fi',
+    totalLoadingTime: 'tl',
+    downloadingTime: 'dl',
+    DOMParsingTime: 'dp',
+    DNSLookupTime: 'du',
+    imagesTime: 'im',
+    stylesTime: 'ss',
+    scriptsTime: 'sc'
 };
 
 export default class PerformanceReporter extends Reporter {
-  private endpoint: string = `//127.0.0.1:3000/perf.gif`;
+    private endpoint: string = `//www.dobest.me:3030/perf.gif`;
 
-  constructor(private token: string, private environment: Environment) {
-    super();
-  }
-
-  report(metric: Metric): Promise<void> {
-    const parameters = this.parameterBuilder(metric);
-    const url = this.URLBuilder(this.endpoint, parameters);
-    LOG('Combined parameters:', parameters);
-    LOG('Report to url:', url);
-    return this.sendRequest(url);
-  }
-
-  private parameterBuilder(metric: Metric) {
-    const parameters = {};
-    const keys = Object.keys(metric);
-    for (const key of keys) {
-      const value = metric[key];
-      const alias = parameterMapper[key];
-      // Filtering invalid metric data
-      if (value && alias && !isNaN(value) && value !== -1) {
-        parameters[alias] = value;
-      }
+    constructor(private token: string, private environment: Environment) {
+        super();
     }
 
-    // Should also carry token string
-    parameters['token'] = this.token;
+    report(metric: Metric): Promise<void> {
+        const parameters = this.parameterBuilder(metric);
+        const url = this.URLBuilder(this.endpoint, parameters);
+        LOG('Combined parameters:', parameters);
+        LOG('Report to url:', url);
+        return this.sendRequest(url);
+    }
 
-    // Combine environment infomation
-    parameters['os'] = this.environment.operatingSystem._type.toLowerCase();
-    parameters['br'] = this.environment.browser._type.toLowerCase();
-    parameters['bv'] = this.environment.browser.version.toLowerCase();
-    parameters['dc'] = this.environment.device.type;
-    parameters['dv'] = this.environment.device.version;
+    private parameterBuilder(metric: Metric) {
+        const parameters = {};
+        const keys = Object.keys(metric);
+        for (const key of keys) {
+            const value = metric[key];
+            const alias = parameterMapper[key];
+            // Filtering invalid metric data
+            if (value && alias && !isNaN(value) && value !== -1) {
+                parameters[alias] = value;
+            }
+        }
 
-    return parameters;
-  }
+        // Should also carry token string
+        parameters['token'] = this.token;
+
+        // Combine environment infomation
+        parameters['os'] = this.environment.operatingSystem._type.toLowerCase();
+        parameters['br'] = this.environment.browser._type.toLowerCase();
+        parameters['bv'] = this.environment.browser.version.toLowerCase();
+        parameters['dc'] = this.environment.device.type;
+        parameters['dv'] = this.environment.device.version;
+
+        return parameters;
+    }
 
 }
